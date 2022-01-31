1cc49d155a1309b4a392ecdee5ea92a1b7779900
barraux.a <barraux.a@example.com>
2022-01-31 21:12:19 +0100

    corection

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 for (let i = 0; i < document.getElementsByClassName('file').length; i++) {
     document.getElementsByClassName('file')[i].addEventListener('contextmenu', function(event){
         event.preventDefault();
-        document.getElementsByClassName('menu')[i].style.visibility = 'visible';
-        document.getElementsByClassName('menu')[i].style.top = event.pageY;
-        document.getElementsByClassName('menu')[i].style.left = event.pageX;
+        document.getElementsByClassName('menu')[i].style.display = 'block';
+        console.log(event.pageX, event.pageY);
+        console.log(event.clientX, event.clientY);
+        document.getElementsByClassName('menu')[i].style.top = event.pageY - document.getElementsByTagName('article')[0].offsetTop;
+        document.getElementsByClassName('menu')[i].style.left = event.pageX - document.getElementsByTagName('article')[0].offsetLeft;
     })
 }
 
-for (let i = 0; i < document.getElementsByClassName('file').length; i++) {
-    document.getElementsByClassName('file')[i].addEventListener('click', function(event){
-        document.getElementById('menu').style.visibility[i] = 'hidden';
-    })
-}
+document.addEventListener('click', function(event){
+    for (let i = 0; i < document.getElementsByClassName('file').length; i++) {
+        document.getElementsByClassName('menu')[i].style.display = 'none';
+    }
+})
